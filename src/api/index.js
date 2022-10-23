00c936e7f752401e0f67b945d0837b57cdcd46f9
Valentino Patane <valentinopatane98@gmail.com>
2022-10-23 20:52:44 -0300

    URL edit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://tired-jade-button.cyclic.app/",
 });
 
 API.interceptors.request.use((req) => {
